297bb838ef16563366fc1faa3725136cab8a92b6
artusvranken <artusvranken@gmail.com>
2017-11-09 19:22:51 +0100

    Added the first class.

---
+++ task.js
@@ -0,0 +1,13 @@
+class Task
+{
+    id: string;
+    description: string;
+    priotity: green;
+    
+    constructor(message : string, priority : string)
+    {
+        this.id = "0"; 
+        this.description = description;
+        this.priority = priority;
+    }
+}
